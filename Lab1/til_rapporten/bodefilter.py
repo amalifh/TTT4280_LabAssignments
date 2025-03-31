030b316ec5b409db3734f8c53801384cc882489e
amalifh <amalifh@stud.ntnu.no>
2025-03-31 18:00:28 +0200

    Med bodeplot for BW-filter

--- Lab1/til_rapporten/bodefilter.py
+++ Lab1/til_rapporten/bodefilter.py
@@ -4,16 +4,17 @@ import matplotlib.pyplot as plt
 #matplotlib inline
 
 # Step 1: Read the CSV file
-data = pd.read_csv('lab1_riktigbode_lavpassfilter.csv')
+data = pd.read_csv('Lab4/Bodeplot_filter2-ingand.csv')
 
 # Step 2: Plot the data
 # You can choose specific columns to plot
-plt.plot(data['Frequency (Hz)'], data['Channel 2 Magnitude (dB)'])
+plt.semilogx(data['Frequency (Hz)'], data['Channel 2 Magnitude (dB)'])
 #plt.plot(data['Time (s)'], data['Channel 2 (V)'], label='v_o')
-plt.xlim(10,70)
-plt.ylim(-20,10)
+plt.xlim(1,4000)
 
-plt.axvline(x=31, color='red', linestyle='--', linewidth=1)
+#plt.ylim(-20,10)
+
+#plt.axvline(x=31, color='red', linestyle='--', linewidth=1)
 
 #plt.axhline(y=8.6,  color='red', linestyle='--', linewidth=1)
 """plt.axhline(y=-3, color='red', linestyle='--', linewidth=1)
@@ -26,12 +27,12 @@ plt.plot(2350, -9.66, marker='o', markersize=7, color='green')"""
 
 plt.xlabel('Frekvens [Hz]')
 plt.ylabel('Amplituderespons [dB]')
-plt.title('Bodediagram for lavpassfilter')
+plt.title('Bodediagram for Butterworth')
 
 plt.grid(True)
 #plt.legend()
 
 # Step 3: Show the plot
-#plt.show()
+plt.show()
 
-plt.savefig('Bodediagram_lavpassfilter.png', dpi = 700)
+#plt.savefig('Bodediagram_filter1_butterworth.png', dpi = 700)

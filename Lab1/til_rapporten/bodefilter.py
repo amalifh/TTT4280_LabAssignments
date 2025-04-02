2e428705ea76ae8afd0e39e0dba745511482a4bd
Caroline Losnedal <carolinelosnedal@dhcp-10-22-55-203.wlan.ntnu.no>
2025-04-02 12:55:56 +0200

    Bodeplot til lab4

--- Lab1/til_rapporten/bodefilter.py
+++ Lab1/til_rapporten/bodefilter.py
@@ -4,17 +4,37 @@ import matplotlib.pyplot as plt
 #matplotlib inline
 
 # Step 1: Read the CSV file
-data = pd.read_csv('Lab4/Bodeplot_filter2-ingand.csv')
+data = pd.read_csv('Lab4/Bodeplot_filter.csv')
 
+freq = data['Frequency (Hz)']
+amp = data['Channel 2 Magnitude (dB)']
 # Step 2: Plot the data
 # You can choose specific columns to plot
-plt.semilogx(data['Frequency (Hz)'], data['Channel 2 Magnitude (dB)'])
+plt.semilogx(freq, amp, label = 'Amplituderespons')
 #plt.plot(data['Time (s)'], data['Channel 2 (V)'], label='v_o')
 plt.xlim(1,4000)
 
-#plt.ylim(-20,10)
+plt.ylim(8,20.3)
 
-#plt.axvline(x=31, color='red', linestyle='--', linewidth=1)
+#Legge til røde striplede linjer ved cutoff
+
+peak = amp.max()
+print(peak)
+cutoff_level = peak - 3.0
+print(cutoff_level)
+
+
+
+plt.plot(3.82,17.18, 'ro') 
+plt.plot(2629, 17.18, 'ro')
+
+plt.axhline(cutoff_level, color='red', linestyle='--', label='-3 dB nivå') # red dot
+
+plt.axvline(x=3.82, color='red', linestyle='--', linewidth=1)
+plt.axvline(x=2629,color='red', linestyle='--', linewidth=1)
+
+plt.xticks(fontsize = 10)
+plt.yticks(fontsize = 10)
 
 #plt.axhline(y=8.6,  color='red', linestyle='--', linewidth=1)
 """plt.axhline(y=-3, color='red', linestyle='--', linewidth=1)
@@ -27,12 +47,12 @@ plt.plot(2350, -9.66, marker='o', markersize=7, color='green')"""
 
 plt.xlabel('Frekvens [Hz]')
 plt.ylabel('Amplituderespons [dB]')
-plt.title('Bodediagram for Butterworth')
+plt.title('Bodediagram for aktivt båndpass med cutoff ved 3.8 Hz og 2.63 kHz', fontsize = 12)
 
 plt.grid(True)
-#plt.legend()
+plt.legend()
 
 # Step 3: Show the plot
-plt.show()
+#plt.show()
 
-#plt.savefig('Bodediagram_filter1_butterworth.png', dpi = 700)
+plt.savefig('Bodediagram_filter1_butterworth.png', dpi = 700)

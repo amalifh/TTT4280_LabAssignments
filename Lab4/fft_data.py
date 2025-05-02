ab2583027404c85d24e9e73a8ab5d172f001d757
Amalie Fridfeldt Hauge <amalifh@ntnu.no>
2025-05-02 12:37:09 +0200

    DET FUNKER

--- Lab4/fft_data.py
+++ Lab4/fft_data.py
@@ -19,40 +19,39 @@ def raspi_import(path, channels=2):
     sample_period *= 1e-6
     return sample_period, data
 
+
 if __name__ == "__main__":
     sample_period, data = raspi_import(sys.argv[1] if len(sys.argv) > 1 else 'Lab4/data/SR/radar_rev4.bin')
-    channel_data = data[:,0] #I-signal
-    channel_data2 = data[:,1] #Q-signal
+    fs = 1 / sample_period
+    start_time = 0.1
+    start_sample = int(start_time * fs)
 
-    I_signal = detrend(channel_data)
-    Q_signal = detrend(channel_data2)
-    window_func = len
+    I_signal = data[start_sample:, 0] - np.mean(data[start_sample:, 0])
+    Q_signal  = data[start_sample:, 1] - np.mean(data[start_sample:, 1])
+
 
     cmpx_signal = I_signal + 1j*Q_signal
 
-    fft_data = np.fft.fft(cmpx_signal)
+    window_func = np.hanning(len(cmpx_signal))
+    cmpx_signal *= window_func
 
-    n = len(cmpx_signal)
-    n_z = 2**15
-    frekvenser = np.fft.fftfreq(n, d=sample_period)
+    N = 2**15
+
+    fft_data = np.fft.fftshift(np.fft.fft(cmpx_signal, n=N))
+    freq = np.fft.fftshift(np.fft.fftfreq(N, d = 1/fs))
 
     magnitude = np.abs(fft_data)
 
-    magnitude_db = 20 * np.log10(magnitude)
-
-    magnitude_db_normalisert = magnitude_db - np.max(magnitude_db)
+    magnitude_db = 20 * np.log10(np.abs(fft_data))
 
     plt.figure(figsize=(10, 6))
-    plt.plot(frekvenser[:n//2], magnitude_db_normalisert[:n//2]) 
-    plt.plot(93.4,-41.1, 'o', color = 'indigo', label = 'Signifikant topp: 86.2 Hz')
-    plt.axhline(y = -69.27, color = 'salmon', linestyle = '--', label = 'esitmert SNR')
+    plt.plot(freq, magnitude_db) 
     plt.xlabel('Frekvens [Hz]')
     plt.ylabel('Amplitude [dB]')
-    plt.xlim(0,400)
-    plt.ylim(-100, 0)
     plt.grid()
-    plt.legend()
+    #plt.legend()
     #plt.ylim(0,400)
     plt.title('Frekvensspekter: negativ hastighet')
-    #plt.show()
-    plt.savefig('frekvens_negativ', dpi = 700)
+    plt.show()
+    #plt.savefig('frekvens_negativ', dpi = 700)
+
